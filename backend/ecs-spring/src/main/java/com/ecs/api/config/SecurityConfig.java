bbf8b7ec742aae10e499fbaf822440fda7e0007a
parksohui <psh990525@naver.com>
2023-04-20 17:23:10 +0900

    [BE] feat: add securityConfig

--- backend/ecs-spring/src/main/java/com/ecs/api/config/SecurityConfig.java
+++ backend/ecs-spring/src/main/java/com/ecs/api/config/SecurityConfig.java
@@ -1,4 +1,44 @@
 package com.ecs.api.config;
 
+import com.ecs.api.config.oauth.CustomOAuthSuccessHandler;
+import com.ecs.api.config.oauth.PrincipalOauth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final PrincipalOauth2UserService principalOauth2UserService;
+    private final CustomOAuthSuccessHandler customOAuthSuccessHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf().ignoringAntMatchers("/h2-console/**").disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .authorizeRequests()
+                .antMatchers("/h2-console/**",
+                        "/webjars/**")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .headers()
+                .frameOptions().sameOrigin()
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService)
+                .and()
+                .successHandler(customOAuthSuccessHandler);
+        return http.build();
+    }
 }
