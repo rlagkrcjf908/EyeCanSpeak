c813ed4181ce4a2b23b17a9d72f1cc73c2f7f0b0
dnjs0236 <dnjs0236@gmail.com>
2023-04-20 17:40:44 +0900

    [FE] feat : board carousel

--- frontend/ecs-react/src/components/board/slide.tsx
+++ frontend/ecs-react/src/components/board/slide.tsx
@@ -1,110 +1,76 @@
-import style from "../../styles/board/slide.module.css"
-import { useEffect } from "react"
+import "../../styles/board/slide.css"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import React, { useState } from "react"
 
 export default function Slide() {
-  let currentIdx = 0
-  let slideCount = 0
-  let slides: any
-  let start = true
-  let slideWidth = 300 //슬라이드이미지 넓이
-  let slideMargin = 100 //슬라이드 끼리의 마진값
-
-  useEffect(() => {
-    if (start === false) return
-    let next: any = document.getElementsByClassName(`${style["next"]}`)[0]
-    next.addEventListener("click", function () {
-      //다음 버튼 눌렀을때
-      if (currentIdx <= slideCount - 1) {
-        //슬라이드이동
-        slides.style.left =
-          -(currentIdx + 2) * (slideWidth + slideMargin) + "px"
-        slides.style.transition = `${0.5}s ease-out` //이동 속도
-      }
-      if (currentIdx === slideCount - 1) {
-        //마지막 슬라이드 일때
-        setTimeout(function () {
-          //0.5초동안 복사한 첫번째 이미지에서, 진짜 첫번째 위치로 이동
-          slides.style.left = -(slideWidth + slideMargin) + "px"
-          slides.style.transition = `${0}s ease-out`
-        }, 500)
-        currentIdx = -1
-      }
-      currentIdx += 1
-    })
-
-    showSlides()
-  }, [])
-
-  function showSlides(): void {
-    start = false
-    slides = document.getElementsByClassName(`${style["slides"]}`)[0]
-    let slideImg: any = document.getElementsByClassName(`${style["item"]}`)
-    currentIdx = 0
-    slideCount = slideImg.length
-
-    makeClone() // 처음이미지와 마지막 이미지 복사 함수
-    initfunction() //슬라이드 넓이와 위치값 초기화 함수
-    function makeClone() {
-      let cloneSlide_first = slides.firstElementChild.cloneNode(true)
-      let cloneSlide_last = slides.lastElementChild.cloneNode(true)
-      slides.append(cloneSlide_first)
-      slides.insertBefore(cloneSlide_last, slides.firstElementChild)
-      console.log(slides)
-    }
-    function initfunction() {
-      slides.style.width = (slideWidth + slideMargin) * (slideCount + 2) + "px"
-      slides.style.left = -(slideWidth + slideMargin) + "px"
-    }
+  const customeSlider: any = React.createRef()
+  const setting = {
+    centerMode: true,
+    infinite: true,
+    centerPadding: "100px",
+    slidesToShow: 1,
+    speed: 500,
+    arrows: false,
+    adaptiveHeight: true,
+  }
+  const gotoNext = () => {
+    customeSlider.current.slickNext()
   }
 
-  const nextEvent = () => {
-    if (currentIdx <= slideCount - 1) {
-      //슬라이드이동
-      slides.style.left = -(currentIdx + 2) * (slideWidth + slideMargin) + "px"
-      slides.style.transition = `${0.5}s ease-out` //이동 속도
-    }
-    if (currentIdx === slideCount - 1) {
-      //마지막 슬라이드 일때
-      setTimeout(function () {
-        //0.5초동안 복사한 첫번째 이미지에서, 진짜 첫번째 위치로 이동
-        slides.style.left = -(slideWidth + slideMargin) + "px"
-        slides.style.transition = `${0}s ease-out`
-      }, 500)
-      currentIdx = -1
-    }
-    currentIdx += 1
-  }
-
-  const prevEvent = () => {
-    if (currentIdx >= 0) {
-      slides.style.left = -currentIdx * (slideWidth + slideMargin) + "px"
-      slides.style.transition = `${0.5}s ease-out`
-    }
-    if (currentIdx === 0) {
-      setTimeout(function () {
-        slides.style.left = -slideCount * (slideWidth + slideMargin) + "px"
-        slides.style.transition = `${0}s ease-out`
-      }, 500)
-      currentIdx = slideCount
-    }
-    currentIdx -= 1
+  const gotoPrev = () => {
+    customeSlider.current.slickPrev()
   }
 
   return (
-    <div id={`${style.slideShow}`}>
-      <ul className={`${style.slides}`}>
-        <li className={`${style.item}`}>111</li>
-        <li className={`${style.item}`}>222</li>
-        <li className={`${style.item}`}>333</li>
-      </ul>
-      <p className={`${style.controller}`}>
-        <span className={`${style.prev}`} onClick={prevEvent}>
-          &lang;
-        </span>
-        <span className={`${style.next}`} onClick={nextEvent}>
-          &rang;
-        </span>
-      </p>
+    <div className='main'>
+      <div>
+        <Slider {...setting} ref={customeSlider} className='slider slider-nav'>
+          <div>
+            <h3>
+              {" "}
+              <img
+                src='https://cdn.pixabay.com/photo/2023/04/11/15/39/art-7917562__340.jpg'
+                alt=''
+              />
+            </h3>
+          </div>
+          <div>
+            <h3>
+              {" "}
+              <img
+                src='https://cdn.pixabay.com/photo/2023/03/22/04/26/pagoda-7868621__340.jpg'
+                alt=''
+              />
+            </h3>
+          </div>
+          <div>
+            <h3>
+              <img
+                src='https://cdn.pixabay.com/photo/2023/03/29/13/19/sea-7885432__340.jpg'
+                alt=''
+              />
+            </h3>
+          </div>
+          <div>
+            <h3>
+              <img
+                src='https://cdn.pixabay.com/photo/2023/03/27/10/53/brutalism-7880446__340.jpg'
+                alt=''
+              />
+            </h3>
+          </div>
+        </Slider>
+      </div>
+      <div className='slide-buttons'>
+        <button className='prev-button' onClick={() => gotoPrev()}>
+          Prev
+        </button>
+        <button className='next-button' onClick={() => gotoNext()}>
+          Next
+        </button>
+      </div>
     </div>
   )
 }
