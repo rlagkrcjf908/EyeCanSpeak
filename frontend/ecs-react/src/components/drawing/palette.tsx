5a764323277fae47dfbe40cc00e6716c597d1ea8
dnjs0236 <dnjs0236@gmail.com>
2023-04-25 14:53:53 +0900

    [FE[ style : draw design

--- frontend/ecs-react/src/components/drawing/palette.tsx
+++ frontend/ecs-react/src/components/drawing/palette.tsx
@@ -7,55 +7,90 @@ export default function Palette({
   changeSize: any
 }) {
   return (
-    <div className={style.container}>
-      <div
-        className={`${style.item} ${style.red}`}
-        onClick={(e: any) => {
-          changeColor("#ff0000")
-        }}
-      ></div>
-      <div
-        className={`${style.item} ${style.orange}`}
-        onClick={() => {
-          changeColor("#ff9900")
-        }}
-      ></div>
-      <div
-        className={`${style.item} ${style.yellow}`}
-        onClick={() => {
-          changeColor("#fef400")
-        }}
-      ></div>
-      <div
-        className={`${style.item} ${style.green}`}
-        onClick={() => {
-          changeColor("#01eb18")
-        }}
-      ></div>
-      <div
-        className={`${style.item} ${style.blue}`}
-        onClick={() => {
-          changeColor("#037fda")
-        }}
-      ></div>
-      <div
-        className={`${style.item} ${style.purple}`}
-        onClick={() => {
-          changeColor("#bf00cf")
-        }}
-      ></div>
-      <div
-        className={`${style.item} ${style.black}`}
-        onClick={() => {
-          changeColor("#bf00cf")
-        }}
-      ></div>
-      <div
-        className={`${style.item} ${style.white}`}
-        onClick={() => {
-          changeColor("#bf00cf")
-        }}
-      ></div>
+    <div>
+      <div className={style.container}>
+        <div
+          className={style.item}
+          onClick={() => {
+            changeColor("#ff0000")
+          }}
+        ></div>
+        <div
+          className={style.item}
+          onClick={() => {
+            changeColor("rgb(255, 98, 185)")
+          }}
+        ></div>
+        <div
+          className={style.item}
+          onClick={() => {
+            changeColor("#ff9900")
+          }}
+        ></div>
+        <div
+          className={style.item}
+          onClick={() => {
+            changeColor("#fef400")
+          }}
+        ></div>
+        <div
+          className={style.item}
+          onClick={() => {
+            changeColor("#01eb18")
+          }}
+        ></div>
+        <div
+          className={style.item}
+          onClick={() => {
+            changeColor("#037fda")
+          }}
+        ></div>
+        <div
+          className={style.item}
+          onClick={() => {
+            changeColor("#bf00cf")
+          }}
+        ></div>
+        <div
+          className={style.item}
+          onClick={() => {
+            changeColor("black")
+          }}
+        ></div>
+        <div
+          className={style.item}
+          onClick={() => {
+            changeColor("white")
+          }}
+        ></div>
+      </div>
+      <div className={style.penText}>선 굵기</div>
+      <div className={style.pens}>
+        <button
+          className={style.pen}
+          onClick={() => {
+            changeSize(2)
+          }}
+        >
+          small
+        </button>
+        <button
+          className={style.pen}
+          onClick={() => {
+            changeSize(5)
+          }}
+        >
+          normal
+        </button>
+        <button
+          className={style.pen}
+          onClick={() => {
+            changeSize(8)
+          }}
+        >
+          large
+        </button>
+      </div>
     </div>
   )
 }
