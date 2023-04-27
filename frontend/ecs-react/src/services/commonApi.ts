7f3885601db4bc91f40e0f02aeaf4541a4bbc569
dnjs0236 <dnjs0236@gmail.com>
2023-04-27 16:39:12 +0900

    [FE] test

--- frontend/ecs-react/src/services/commonApi.ts
+++ frontend/ecs-react/src/services/commonApi.ts
@@ -2,6 +2,8 @@ import { AxiosResponse } from "axios"
 import customAxios from "./api"
 
 export async function getCategory() {
+  console.log("??")
   const response: AxiosResponse = await customAxios.get(`/draw/category`)
+
   return response
 }
