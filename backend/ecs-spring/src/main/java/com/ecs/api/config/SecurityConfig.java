9728589a9f38c1facd5a1b08382ede05c545811f
parksohui <psh990525@naver.com>
2023-04-27 15:58:15 +0900

    [BE] refactor: modify cors

--- backend/ecs-spring/src/main/java/com/ecs/api/config/SecurityConfig.java
+++ backend/ecs-spring/src/main/java/com/ecs/api/config/SecurityConfig.java
@@ -58,11 +58,12 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration corsConfiguration=new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of(
-                "https://k8d204.p.ssafy.io/",
-                "http://localhost:3000",
-                "http://192.168.100.85:3000",
-                "http://192.168.100.190:3000"));
+//        corsConfiguration.setAllowedOrigins(List.of(
+//                "https://k8d204.p.ssafy.io/",
+//                "http://localhost:3000",
+//                "http://192.168.100.85:3000/",
+//                "http://192.168.100.190:3000/"));
+        corsConfiguration.addAllowedOrigin("*");
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.setAllowedHeaders(List.of("Accept", "Accept-Language", "Authorization", "Content-Language", "Content-Type"));
         corsConfiguration.setAllowCredentials(true);
