d490a2d909ef00bcfe0d45871b3fa4c2a356b136
dnjs0236 <dnjs0236@gmail.com>
2023-04-27 15:25:02 +0900

    [FE] refactor : api url

--- frontend/ecs-react/src/services/commonApi.ts
+++ frontend/ecs-react/src/services/commonApi.ts
@@ -4,6 +4,6 @@ import axios from "axios"
 import customAxios from "./api"
 
 export async function getCategory() {
-  const response: AxiosResponse = await axios.get(`/draw/category`)
+  const response: AxiosResponse = await customAxios.get(`/draw/category`)
   return response
 }
