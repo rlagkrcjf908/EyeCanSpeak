0524c0faff9b3cebbb89b3c12b43e2c9eaa3fc3e
Ajin <lany1515@naver.com>
2023-04-28 14:43:06 +0900

    [FE] edit:api url

--- frontend/ecs-react/src/services/writingApi.ts
+++ frontend/ecs-react/src/services/writingApi.ts
@@ -5,7 +5,7 @@ const user_no: number = 1
 // 서버단어검색하기
 export async function getWords(payload: string) {
   const response: AxiosResponse = await customAxios.post(
-    `/write/history?user_no=${user_no}/`,
+    `/write/history?user_no=${user_no}`,
     {
       write_content: payload,
     }
@@ -15,7 +15,7 @@ export async function getWords(payload: string) {
 // 로컬단어검색하기
 export async function saveWord(payload: string) {
   const response: AxiosResponse = await customAxios.post(
-    `/write?user_no=${user_no}/`,
+    `/write?user_no=${user_no}`,
     {
       write_content: payload,
     }
