591e89b7b5bcb00fb5ad3bf84c2e196c228a63c3
dnjs0236 <dnjs0236@gmail.com>
2023-05-02 15:20:10 +0900

    [FE] test

--- frontend/ecs-react/src/components/drawing/board.tsx
+++ frontend/ecs-react/src/components/drawing/board.tsx
@@ -146,14 +146,14 @@ function Board({ width, height }: CanvasProps) {
   }
 
   const saveApi = async (formData: FormData) => {
-    // const token = cookies.get("accessToken")
+    const token = cookies.get("accessToken")
     const response: AxiosResponse = await axios.post(
       "https://k8d204.p.ssafy.io/api/draw/store",
       formData,
       {
         headers: {
           "Content-Type": "multipart/form-data",
-          // Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${token}`,
         },
       }
     )
