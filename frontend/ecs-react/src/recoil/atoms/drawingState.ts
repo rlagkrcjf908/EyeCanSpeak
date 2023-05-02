8d41d7cd00180bfd09801f14c226355a97631e43
dnjs0236 <dnjs0236@gmail.com>
2023-05-02 16:57:47 +0900

    [FE] feat : image download

--- frontend/ecs-react/src/recoil/atoms/drawingState.ts
+++ frontend/ecs-react/src/recoil/atoms/drawingState.ts
@@ -4,12 +4,3 @@ export const bgImg = atom<string>({
   key: "bgImg",
   default: "",
 })
-
-interface drawInfoType {
-  draw_no: number
-  subject_nm: string | null
-}
-export const drawInfoState = atom<drawInfoType>({
-  key: "drawInfo",
-  default: { draw_no: -1, subject_nm: null },
-})
