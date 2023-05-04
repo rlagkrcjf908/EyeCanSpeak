78b6340692f29f199f2d673a683b7a38f60734c3
parksohui <psh990525@naver.com>
2023-05-04 10:02:33 +0900

    [BE] fix: modify token expire date

--- backend/ecs-spring/src/main/java/com/ecs/api/config/oauth/CustomOAuthSuccessHandler.java
+++ backend/ecs-spring/src/main/java/com/ecs/api/config/oauth/CustomOAuthSuccessHandler.java
@@ -33,13 +33,13 @@ public class CustomOAuthSuccessHandler implements AuthenticationSuccessHandler {
 
         Cookie accessCookie=new Cookie("accessToken", jwtToken);
         accessCookie.setPath("/");
-        accessCookie.setMaxAge(1000 * 60 * 60);
+        accessCookie.setMaxAge(60 * 60);
 
         Cookie refreshCookie=new Cookie("refreshToken", refreshToken);
         refreshCookie.setPath("/");
         refreshCookie.setSecure(true);
         refreshCookie.setHttpOnly(true);
-        refreshCookie.setMaxAge(1000*60*60*24);
+        refreshCookie.setMaxAge(60*60*24);
 
         response.addCookie(accessCookie);
         response.addCookie(refreshCookie);
