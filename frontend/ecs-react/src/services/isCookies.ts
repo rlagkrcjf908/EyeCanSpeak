fc8fb5bc716edf0b6bcec4dc656a498b9d40b92b
dnjs0236 <dnjs0236@gmail.com>
2023-05-08 11:55:45 +0900

    [FE] fix : time out process

--- frontend/ecs-react/src/services/isCookies.ts
+++ frontend/ecs-react/src/services/isCookies.ts
@@ -1,15 +1,9 @@
 import { Cookies } from "react-cookie"
-import { useNavigate } from "react-router-dom"
 
 export default function IsCookies() {
   const cookies = new Cookies()
-  // const navigate = useNavigate()
 
   if (cookies.get("accessToken") === undefined) {
-    console.log(cookies.get("accessToken"))
-    console.log(typeof cookies.get("accessToken"))
-    console.log("time out !!")
     document.location.href = "https://k8d204.p.ssafy.io"
-    // navigate("/")
   }
 }
