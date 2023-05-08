a609b574dec89b38bb4fafe676256c667a3d766c
Chanseok <ckstjr93@naver.com>
2023-05-08 14:12:19 +0900

    [AI] feat: Socket and Gaze Tracking

--- backend/ecs-flask/app.py
+++ backend/ecs-flask/app.py
@@ -1,7 +1,97 @@
-from flask import Flask
+import base64
+import io
+import random
+import cv2
+import numpy as np
+from PIL import Image
+from gaze_tracking.example import Example
+
+
+from flask import Flask, request, jsonify
+from flask_socketio import SocketIO, emit, join_room, leave_room
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+CORS(app, resources={r"/*": {"origins": "*"}})
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-@app.route('/')
-def hello():
-    return 'Hello, Flask!'
+
+@app.route("/http-call")
+def http_call():
+    """return JSON with string data as the value"""
+    data = {'data': 'This text was fetched using an HTTP call to server on render'}
+    return jsonify(data)
+
+
+@socketio.on("connect")
+def connected():
+    """event listener when client connects to the server"""
+    room = request.sid
+    print(room)
+    print("client has connected")
+
+    join_room(room)
+    emit("connect", {"data": f"id: {request.sid} is connected"}, room=room)
+
+
+@socketio.on('data')
+def handle_message(data):
+    """event listener when client types a message"""
+    print("data from the front end: ", str(data))
+    room = request.sid
+    emit("data", {'data': data, 'id': request.sid}, room=room)
+
+
+@socketio.on('data')
+def handle_message(data):
+    """event listener when client types a message"""
+    # 랜덤x,y
+
+    x = random.randrange(1, 500)
+    y = random.randrange(1, 500)
+
+    print("data from the front end: ", str(data))
+    room = request.sid
+    emit("data", {'data': data, 'id': request.sid, 'x': x, 'y': y}, room=room)
+
+
+@socketio.on('imageConversionByClient')
+def handle_image(image):
+    """event listener when client types a message"""
+    # print("image from the front end: ", str(image))
+    room = request.sid
+
+    base_str = image['buffer'].split(',')[1]
+    im_bytes = base64.b64decode(base_str)
+    im_arr = np.frombuffer(im_bytes, dtype=np.uint8)
+    img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
+    # cv2.imshow("test", img)
+    example = Example()
+    X, Y, DIR = example.test(img)
+    print(f"x: {X} y: {Y}, dir:{DIR}")
+    emit("image", {'image': image, 'id': request.sid, 'x': X, 'y': Y, 'dir': DIR}, room=room)
+
+
+@socketio.on('test')
+def handle_image(data):
+    """event listener when client types a message"""
+    print("data from the front end: ", str(data))
+    room = request.sid
+
+    emit("test", {'data': data, 'id': request.sid}, room=room)
+
+
+@socketio.on("disconnect")
+def disconnected():
+    """event listener when client disconnects to the server"""
+    print("user disconnected")
+    room = request.sid
+    leave_room(room)
+    emit("disconnect", f"user {request.sid} disconnected", room=room)
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, host='0.0.0.0', port=5001)
+
---
+++ backend/ecs-flask/gaze_tracking/example.py
@@ -0,0 +1,89 @@
+"""
+Demonstration of the GazeTracking library.
+Check the README.md for complete documentation.
+"""
+
+import cv2
+import numpy as np
+
+from gaze_tracking import GazeTracking
+
+class Example(object):
+    def __init__(self):
+        self.frame = ""
+        self.minX = 504.5
+        self.maxX = 512.5
+        self.minY = 324.5
+        self.maxY = 327.5
+        self.diffX = self.maxX - self.minX
+        self.diffY = self.maxY - self.minY
+        self.W = 1024
+        self.H = 768
+
+    def test(self, image):
+        frame = image
+
+        gaze = GazeTracking()
+        src = np.zeros((self.H, self.W, 3), np.uint8)
+
+        # print(type(frame))
+        if(type(frame) != type(None)):
+            print(frame.shape)
+        # We send this frame to GazeTracking to analyze it
+        gaze.refresh(frame)
+
+        frame = gaze.annotated_frame()
+        text = ""
+
+        # if gaze.is_blinking():
+        #     text = "Blinking"
+        # elif gaze.is_right():
+        #     text = "Looking right"
+        # elif gaze.is_left():
+        #     text = "Looking left"
+        # elif gaze.is_center():
+        #     text = "Looking center"
+        #
+        # cv2.putText(frame, text, (150, 100),
+        #             cv2.FONT_HERSHEY_DUPLEX, 0.7, (0, 0, 255), 1)
+
+        left_pupil = gaze.pupil_left_coords()
+        right_pupil = gaze.pupil_right_coords()
+        # cv2.putText(frame, "Left pupil:  " + str(left_pupil), (150, 30),
+        #             cv2.FONT_HERSHEY_DUPLEX, 0.7, (0, 0, 255), 1)
+        # cv2.putText(frame, "Right pupil: " + str(right_pupil), (150, 65),
+        #             cv2.FONT_HERSHEY_DUPLEX, 0.7, (0, 0, 255), 1)
+        #
+        # cv2.putText(frame, "horizontal_ratio: " + str(gaze.horizontal_ratio()), (150, 100),
+        #             cv2.FONT_HERSHEY_DUPLEX, 0.7, (0, 0, 255), 1)
+        # cv2.putText(frame, "vertical_ratio: " + str(gaze.vertical_ratio()), (150, 135),
+        #             cv2.FONT_HERSHEY_DUPLEX, 0.7, (0, 0, 255), 1)
+
+        nx, ny = -1, -1
+        if(type(left_pupil) != type(None) and type(right_pupil) != type(None)):
+            x = (left_pupil[0] + right_pupil[0]) / 2
+            y = (left_pupil[1] + right_pupil[1]) / 2
+            print(f'X : {x}, Y: {y}')
+            print(f'self.minX : {self.minX}, self.diffX: {self.diffX }')
+            print(f'self.minY : {self.minY}, self.diffY: {self.diffY}')
+
+            print(f'src.shape[0] : {src.shape[0]}, src.shape[1]: {src.shape[1]}\n')
+
+            nx = src.shape[1] - (x - self.minX) / self.diffX * src.shape[1]
+            ny = (y - self.minY) / self.diffY * src.shape[0]
+            print(f'nX : {nx:.2f}, nY: {ny:.2f}\n')
+            # src = cv2.circle(src, (int(nx), int(ny)), 5, (0, 255, 0), cv2.FILLED, cv2.LINE_4)
+            # cv2.imshow("draw", src)
+            # src = cv2.circle(src, (int(nx), int(ny)), 5, (0, 0, 0), cv2.FILLED, cv2.LINE_4)
+
+        # cv2.namedWindow('Demo', cv2.WINDOW_NORMAL)
+        # dst = cv2.resize(frame, (0, 0), 2, 4, cv2.INTER_LINEAR)
+        # cv2.imshow("Demo", frame)
+        # cv2.imshow("draw", src)
+        # print(frame.shape)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+        return nx, ny, 5
+
+
