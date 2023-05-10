bde8be050c65c037bb8ed8939a458c8bb3863aec
dnjs0236 <dnjs0236@gmail.com>
2023-05-10 14:18:39 +0900

    [FE] feat : setting page

--- frontend/ecs-react/src/pages/setting.tsx
+++ frontend/ecs-react/src/pages/setting.tsx
@@ -3,15 +3,23 @@ import { useState, useEffect, useRef } from "react"
 import Webcam from "react-webcam"
 import { Cookies } from "react-cookie"
 import axios, { AxiosResponse } from "axios"
+import { useRecoilValue } from "recoil"
+import { userNo } from "../recoil/atoms/userState"
 export default function Setting() {
   const [count, setCount] = useState<number>(5)
-  const [circleStyle, setCircleStyle] = useState({ top: "0", left: "0" })
   const webcamRef = useRef<Webcam>(null)
-  const [imgSrc, setImgSrc] = useState<string[]>([])
   const [TestimgSrc, setTestImgSrc] = useState<string>("")
-
+  const [currentCircle, setCurrentCircle] = useState(0)
   const cookies = new Cookies()
+  const userNumber = useRecoilValue(userNo)
 
+  // 동그라미 위치 (좌상/우상/좌하/우하)
+  const circles = [
+    { top: "0", left: "0" },
+    { top: "0", left: "calc(100% - 100px)" },
+    { top: "calc(100% - 100px)", left: "0" },
+    { top: "calc(100% - 100px)", left: "calc(100% - 100px)" },
+  ]
   // 캠 화면, 나중에 안보이게 수정
   const videoConstraints = {
     width: 420,
@@ -19,38 +27,23 @@ export default function Setting() {
   }
 
   // 캡쳐화면 imgSrc에 저장
-  const capture = () => {
+  const capture = async () => {
     if (!webcamRef.current) return
     //캡쳐된 이미지
     const imageSrc: string | null = webcamRef.current.getScreenshot()
     if (!imageSrc) return
-    setImgSrc((imgSrc) => [...imgSrc, imageSrc])
-    setTestImgSrc(imageSrc)
-    // console.log(1)
-    console.log(imageSrc)
-  }
-
-  useEffect(() => {
-    console.log(imgSrc)
-    if (imgSrc.length === 4) sendImage()
-  }, [imgSrc])
-
-  // 캡쳐보내기
-  const sendImage = async () => {
-    console.log("sendImage")
-    console.log("----------------------", imgSrc)
+    console.log("sendimage")
     const token = cookies.get("accessToken")
     // const formData: FormData = new FormData()
     // imgSrc.forEach((element) => {
     //   formData.append("settingImg", element)
     // })
-    console.log("****************", TestimgSrc)
     const response: AxiosResponse = await axios.post(
       // api 주소 적기
       // `https://k8d204.p.ssafy.io/api/?????`,
-      `http://192.168.100.191:5001/setting`,
+      `http://192.168.100.88:5000/setting`,
       // { imgSrc }
-      { imgSrc: TestimgSrc }
+      { userNo: userNumber, imgSrc: imageSrc, index: currentCircle + 1 }
       // formData,
       // {
       //   headers: {
@@ -60,58 +53,46 @@ export default function Setting() {
       // }
     )
 
-    if (response.status === 200) {
-      console.log("저장")
-      console.log(response)
-      setImgSrc([])
-    } else {
-      console.log("ERROR")
+    if (response.data === 200) {
+      setCurrentCircle((c) => c + 1)
+    }
+    if (response.data === 400) {
+      console.log("error")
     }
   }
 
-  // 동그라미 위치 (좌상/우상/좌하/우하)
-  const circles = [
-    { top: "0", left: "0" },
-    { top: "0", left: "calc(100% - 100px)" },
-    { top: "calc(100% - 100px)", left: "0" },
-    { top: "calc(100% - 100px)", left: "calc(100% - 100px)" },
-  ]
+  // 캡쳐보내기
+  const sendImage = async () => {}
 
-  const circleCount = circles.length
   /* circles 인덱스번호*6초 후에 각 위치의 동그라미 나타나고 5초부터 0초까지 카운트다운, 카운트가 다 끝나기 전에 화면 캡쳐*/
-  const createCircle = async () => {
-    for (let i = 0; i < circleCount; i++) {
-      const circle = circles[i]
-      setTimeout(() => {
-        setCount(5)
+  useEffect(() => {
+    if (count === -1) return
+    const id = setInterval(() => {
+      setCount((c) => c - 1)
+    }, 1000)
 
-        setTimeout(() => {
-          capture()
-          // }, 4000)
-        }, 2000)
-
-        const countInterval = setInterval(() => {
-          setCount((prevCount) => prevCount - 1)
-        }, 1000)
-
-        setTimeout(() => {
-          clearInterval(countInterval)
-          // }, 5000)
-        }, 3000)
-
-        setCircleStyle(circle)
-        // }, i * 6000)
-      }, i * 3000)
+    if (count === 0) {
+      setCount(5)
+      clearInterval(id)
+      capture()
+      sendImage()
     }
-  }
+    if (currentCircle === 4) {
+      clearInterval(id)
+      return
+    }
+    return () => clearInterval(id)
+  }, [count, currentCircle])
 
   useEffect(() => {
-    createCircle()
-  }, [])
+    if (currentCircle === 4) {
+      setCount(-1)
+    }
+  }, [currentCircle])
 
   return (
     <div className={style.container}>
-      <div className={style.circle} style={circleStyle}>
+      <div className={style.circle} style={circles[currentCircle]}>
         {count}
       </div>
       <Webcam
@@ -125,7 +106,7 @@ export default function Setting() {
         screenshotFormat='image/jpeg'
         videoConstraints={videoConstraints}
       />
-      <button onClick={capture}>Capture photo</button>
+      <div className={style.text}>빨간 원을 쳐다봐주세요</div>
     </div>
   )
 }
