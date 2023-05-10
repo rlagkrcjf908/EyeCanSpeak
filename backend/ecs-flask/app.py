d85093a2aaf9a65b30f8973e93e88321927d1091
Chanseok <ckstjr93@naver.com>
2023-05-10 16:41:32 +0900

    [AI] feat: setting function

--- backend/ecs-flask/app.py
+++ backend/ecs-flask/app.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 from PIL import Image
 from gaze_tracking.example import Example
-
+from gaze_tracking import GazeTracking
 
 from flask import Flask, request, jsonify
 from flask_socketio import SocketIO, emit, join_room, leave_room
@@ -16,6 +16,81 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 CORS(app, resources={r"/*": {"origins": "*"}})
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+setting_point = {}
+user_object = {}
+
+class point(object):
+    def __init__(self, minX, maxX, minY, maxY):
+        self.minX = minX
+        self.maxX = maxX
+        self.minY = minY
+        self.maxY = maxY
+
+def calc_cood(points):
+    minX, maxX, minY, maxY = 1e9, 0, 1e9, 0
+
+    for x, y in points:
+
+        if x > maxX: maxX = x
+        if x < minX: minX = x
+        if y > maxY: maxY = y
+        if y < minY: minY = y
+
+    return minX, maxX, minY, maxY
+
+def setObject(userNo):
+
+    minX, maxX, minY, maxY = calc_cood(setting_point[userNo])
+
+    example = Example(point(minX, maxX, minY, maxY))
+    user_object[userNo] = example
+
+
+#setting
+@app.route("/setting", methods = ['POST'])
+def setting():
+    # room = request.sid
+    # print(room)
+    userNo = request.json.get('userNo')
+    print(userNo)
+    index = request.json.get('index')
+    print(index)
+    image = request.json.get('imgSrc')
+    print(image)
+
+    base_str = image.split(',')[1]
+    im_bytes = base64.b64decode(base_str)
+    im_arr = np.frombuffer(im_bytes, dtype=np.uint8)
+    img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
+
+    x, y = getSettingPoint(img)
+    if x == -1 or y == -1: return jsonify(400)
+    print(x, y)
+
+    if index == 1:
+        setting_point[userNo] = [(0, 0), (0, 0), (0, 0), (0, 0)]
+
+    setting_point[userNo][index - 1] = (x, y)
+    if index == 4:
+        setObject(userNo)
+
+    return jsonify(200)
+
+def getSettingPoint(image):
+
+    gaze = GazeTracking()
+    gaze.refresh(image)
+
+    left_pupil = gaze.pupil_left_coords()
+    right_pupil = gaze.pupil_right_coords()
+
+    x, y = -1, -1
+    if(type(left_pupil) != type(None) and type(right_pupil) != type(None)):
+        x = (left_pupil[0] + right_pupil[0]) / 2
+        y = (left_pupil[1] + right_pupil[1]) / 2
+
+    return x, y
 
 
 @app.route("/http-call")
@@ -43,7 +118,6 @@ def handle_message(data):
     room = request.sid
     emit("data", {'data': data, 'id': request.sid}, room=room)
 
-
 @socketio.on('data')
 def handle_message(data):
     """event listener when client types a message"""
@@ -56,32 +130,28 @@ def handle_message(data):
     room = request.sid
     emit("data", {'data': data, 'id': request.sid, 'x': x, 'y': y}, room=room)
 
-
 @socketio.on('imageConversionByClient')
 def handle_image(image):
     """event listener when client types a message"""
     # print("image from the front end: ", str(image))
     room = request.sid
+    userNo = image['userNo']
+    print("imageConversionByClient:::", userNo)
 
+    # base64 String to Image
     base_str = image['buffer'].split(',')[1]
     im_bytes = base64.b64decode(base_str)
     im_arr = np.frombuffer(im_bytes, dtype=np.uint8)
     img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
     # cv2.imshow("test", img)
-    example = Example()
-    X, Y, DIR = example.test(img)
+
+    # Get pupil point
+    example = user_object[userNo]
+    X, Y, DIR = example.getPupilPoint(img)
     print(f"x: {X} y: {Y}, dir:{DIR}")
     emit("image", {'image': image, 'id': request.sid, 'x': X, 'y': Y, 'dir': DIR}, room=room)
+    # emit("image", {'image': image, 'id': request.sid, 'x': 0, 'y': 0, 'dir': 0}, room=room)
     # emit("image", {'id': request.sid, 'x': X, 'y': Y, 'dir': DIR}, room=room)
-
-
-@socketio.on('test')
-def handle_image(data):
-    """event listener when client types a message"""
-    print("data from the front end: ", str(data))
-    room = request.sid
-
-    emit("test", {'data': data, 'id': request.sid}, room=room)
 
 
 @socketio.on("disconnect")
--- backend/ecs-flask/gaze_tracking/example.py
+++ backend/ecs-flask/gaze_tracking/example.py
@@ -2,6 +2,7 @@
 Demonstration of the GazeTracking library.
 Check the README.md for complete documentation.
 """
+import math
 
 import cv2
 import numpy as np
@@ -9,28 +10,43 @@ import numpy as np
 from gaze_tracking import GazeTracking
 
 class Example(object):
-    def __init__(self):
-        self.frame = ""
-        self.minX = 314.5
-        self.maxX = 324.0
-        self.minY = 215.5
-        self.maxY = 217.5
+    def __init__(self, point):
+        self.minX = point.minX
+        self.maxX = point.maxX
+        self.minY = point.minY
+        self.maxY = point.maxY
         self.diffX = self.maxX - self.minX
         self.diffY = self.maxY - self.minY
         self.W = 640
         self.H = 480
 
-    def test(self, image):
-        frame = image
+    def getPoint(self):
+        return self.minX, self.maxX, self.minY, self.maxY
 
+    def calc_dir(self, x, y):
+
+        dw = self.w / 3
+        dh = self.h / 3
+
+        if 0 <= x < dw and 0 <= y < dh: return 1
+        elif dw <= x < dw*2 and 0 <= y < dh: return 2
+        elif dw*2 <= x < dw*3 and 0 <= y < dh: return 3
+        elif 0 <= x < dw and dh <= y < dh*2: return 4
+        elif dw <= x < dw * 2 and dh <= y < dh*2: return 5
+        elif dw * 2 <= x < dw * 3 and dh <= y < dh*2: return 6
+        elif 0 <= x < dw and dh*2 <= y < dh*3: return 7
+        elif dw <= x < dw * 2 and dh*2 <= y < dh*3: return 8
+        elif dw * 2 <= x < dw * 3 and dh*2 <= y < dh*3: return 9
+
+    def getPupilPoint(self, image):
         gaze = GazeTracking()
-        src = np.zeros((self.H, self.W, 3), np.uint8)
+        # src = np.zeros((self.H, self.W, 3), np.uint8)
 
         # print(type(frame))
-        if(type(frame) != type(None)):
-            print(frame.shape)
+        if(type(image) != type(None)):
+            print(image.shape)
         # We send this frame to GazeTracking to analyze it
-        gaze.refresh(frame)
+        gaze.refresh(image)
 
         frame = gaze.annotated_frame()
         text = ""
@@ -64,13 +80,13 @@ class Example(object):
             x = (left_pupil[0] + right_pupil[0]) / 2
             y = (left_pupil[1] + right_pupil[1]) / 2
             print(f'X : {x}, Y: {y}')
-            print(f'self.minX : {self.minX}, self.diffX: {self.diffX }')
-            print(f'self.minY : {self.minY}, self.diffY: {self.diffY}')
+            # print(f'self.minX : {self.minX}, self.diffX: {self.diffX }')
+            # print(f'self.minY : {self.minY}, self.diffY: {self.diffY}')
 
-            print(f'src.shape[0] : {src.shape[0]}, src.shape[1]: {src.shape[1]}\n')
+            # print(f'src.shape[0] : {src.shape[0]}, src.shape[1]: {src.shape[1]}\n')
 
-            nx = src.shape[1] - (x - self.minX) / self.diffX * src.shape[1]
-            ny = (y - self.minY) / self.diffY * src.shape[0]
+            nx = self.W - (x - self.minX) / self.diffX * self.W
+            ny = (y - self.minY) / self.diffY * self.H
             print(f'nX : {nx:.2f}, nY: {ny:.2f}\n')
             # src = cv2.circle(src, (int(nx), int(ny)), 5, (0, 255, 0), cv2.FILLED, cv2.LINE_4)
             # cv2.imshow("draw", src)
@@ -84,6 +100,10 @@ class Example(object):
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
 
-        return nx, ny, 5
+        nx = math.max(nx, 0)
+        nx = math.min(nx, self.W)
+        ny = math.max(ny, 0)
+        ny = math.min(ny, self.H)
+        print(f'nX : {nx:.2f}, nY: {ny:.2f}\n')
 
-
+        return nx, ny, self.calc_dir(nx, ny)
