0b0349a0d9364bafa15c9da640327d68536bfc9e
dnjs0236 <dnjs0236@gmail.com>
2023-05-12 09:55:01 +0900

    [FE] test : edit setting time

--- frontend/ecs-react/src/pages/setting.tsx
+++ frontend/ecs-react/src/pages/setting.tsx
@@ -67,7 +67,7 @@ export default function Setting() {
     if (count === -1) return
     const id = setInterval(() => {
       setCount((c) => c - 1)
-    }, 1000)
+    }, 300)
 
     if (count === 0) {
       clearInterval(id)
