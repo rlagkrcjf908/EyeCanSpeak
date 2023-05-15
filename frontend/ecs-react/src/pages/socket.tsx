25929b93e42b30013d3ecbab1d55a6acb58976d6
dnjs0236 <dnjs0236@gmail.com>
2023-05-15 17:52:21 +0900

    [FE] test : api test

--- frontend/ecs-react/src/pages/socket.tsx
+++ frontend/ecs-react/src/pages/socket.tsx
@@ -6,16 +6,19 @@ import Webcam from "react-webcam"
 import { useRecoilState, useRecoilValue } from "recoil"
 import { settingState, userNo } from "../recoil/atoms/userState"
 import { Cookies } from "react-cookie"
+import { base64toFile } from "../services/baseToFile"
+import axios, { AxiosResponse } from "axios"
+import image from "../assets/image/eye3.png"
+import { file } from "@babel/types"
+import { async } from "q"
 
 function SocketTest() {
   const [socketInstance, setSocketInstance] = useState<Socket>()
   const [loading, setLoading] = useState(true)
   const webcamRef = useRef<Webcam>(null)
-  const isSetting = useRecoilValue(settingState)
+  const [isSetting, setIsSetting] = useRecoilState(settingState)
   const userNumber = useRecoilValue(userNo)
-  const [start, setStart] = useState(false)
-
-  const [isSet, setIsSet] = useRecoilState(settingState)
+  const [start, setStart] = useState(true)
 
   // 캠 화면
   const videoConstraints = {
@@ -23,67 +26,58 @@ function SocketTest() {
     height: 480,
     // facingMode: { exact: "environment" }
   }
+
   // 캠 화면 캡쳐하고 보냄
-  const capture = () => {
+  const capture = async () => {
+    console.log("send")
     if (!webcamRef.current) return
     const imageSrc = webcamRef.current.getScreenshot()
-
-    if (imageSrc == null) return
-    console.log("send")
-    socketInstance?.emit("imageConversionByClient", {
-      image: true,
-      buffer: imageSrc,
-      userNo: userNumber,
-    })
+    if (imageSrc) send(imageSrc)
   }
 
-  // 소켓 연결
-  useEffect(() => {
-    if (isSetting) {
-      const socket = io("https://k8d204.p.ssafy.io", {
-        // const socket = io("http://192.168.100.207:5000", {
-        path: "/socket.io",
-        // transports: ["websocket"],
-        // cors: {
-        //   origin: "http://localhost:3000/",
-        // },
-      })
+  // const send = async (imageSrc: string) => {
+  //   // base64로 보냄
 
-      const cookies = new Cookies()
-      cookies.set("isSocket", true)
-      console.log(cookies.get("isSocket"))
+  //   const response: AxiosResponse = await axios.post(
+  //     "http://192.168.100.207:5000/flask/position",
+  //     { image: true, buffer: imageSrc, userNo: -1 }
+  //   )
+  //   if (response.status === 200) {
+  //     console.log(response.data)
+  //     capture()
+  //   }
+  // }
 
-      setSocketInstance(socket)
+  const send = async (imageSrc: string) => {
+    // formdata로 보냄
 
-      socket.on("connect", () => {
-        console.log("connect")
-        if (socket.connected) {
-          setStart(true)
-        }
-      })
+    var file = base64toFile(imageSrc, "image_file.png")
 
-      setLoading(false)
+    const formdata = new FormData()
+    formdata.append("image", file)
+    formdata.append("buffer", file)
 
-      socket.on("disconnect", (data: any) => {
-        console.log(data)
-      })
-
-      return function cleanup() {
-        socket.disconnect()
+    const response: AxiosResponse = await axios.post(
+      "http://192.168.100.207:5000/flask/position",
+      { formdata: formdata },
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
       }
-    }
-  }, [isSetting])
+    )
+  }
 
   useEffect(() => {
     if (start) {
-      setTimeout(capture, 3000)
-      capture()
+      setTimeout(capture, 1000)
+      setStart(false)
     }
   }, [start])
 
   useEffect(() => {
     const cookies = new Cookies()
-    if (cookies.get("isSocket")) setIsSet(true)
+    if (cookies.get("isSocket")) setIsSetting(true)
   }, [])
 
   const click = () => {
