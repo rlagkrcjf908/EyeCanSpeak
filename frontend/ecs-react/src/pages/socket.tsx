c35ec00db7440bd4cf05dc271a641f77f9060118
백소원 <dnjs0236@gmail.com>
2023-05-16 00:22:21 +0900

    [FE] test : binary

--- frontend/ecs-react/src/pages/socket.tsx
+++ frontend/ecs-react/src/pages/socket.tsx
@@ -3,14 +3,12 @@ import WebSocketCall from "../components/socket/webSocketCall"
 import { io, Socket } from "socket.io-client"
 import { useRef, useEffect, useState } from "react"
 import Webcam from "react-webcam"
-import { constSelector, useRecoilState, useRecoilValue } from "recoil"
+import { useRecoilState, useRecoilValue } from "recoil"
 import { settingState, userNo } from "../recoil/atoms/userState"
 import { Cookies } from "react-cookie"
 import { base64toFile } from "../services/baseToFile"
+import { base64ToBinary } from "../services/baseToBinary"
 import axios, { AxiosResponse } from "axios"
-import image from "../assets/image/eye3.png"
-import { file } from "@babel/types"
-import { async } from "q"
 
 function SocketTest() {
   const [socketInstance, setSocketInstance] = useState<Socket>()
@@ -48,17 +46,36 @@ function SocketTest() {
   //   }
   // }
 
+  // const send = async (imageSrc: string) => {
+  //   // formdata로 보냄
+
+  //   var file = base64toFile(imageSrc, "image_file.png")
+
+  //   const formdata = new FormData()
+  //   formdata.append("data", JSON.stringify({ image: true, buffer: file, userNo: -1 }))
+
+  //   const response: AxiosResponse = await axios.post(
+  //     "http://192.168.100.207:5000/flask/position",
+  //     { formdata: formdata },
+  //     {
+  //       headers: {
+  //         "Content-Type": "multipart/form-data",
+  //       },
+  //     }
+  //   )
+
+  //   console.log(response)
+  // }
+
   const send = async (imageSrc: string) => {
-    // formdata로 보냄
+    // binary code로 보냄
 
-    var file = base64toFile(imageSrc, "image_file.png")
+    const binary = base64ToBinary(imageSrc)
 
-    const formdata = new FormData()
-    formdata.append("data", JSON.stringify({ image: true, buffer: file, userNo: -1 }))
-
+    console.log(binary)
     const response: AxiosResponse = await axios.post(
       "http://192.168.100.207:5000/flask/position",
-      { formdata: formdata },
+      { image: true, buffer: binary, userNo: -1 },
       {
         headers: {
           "Content-Type": "multipart/form-data",
