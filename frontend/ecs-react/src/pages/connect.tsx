ee57d8e0febb34dc01471b076737121a071ba799
dnjs0236 <dnjs0236@gmail.com>
2023-05-18 17:53:48 +0900

    [FE] test

--- frontend/ecs-react/src/pages/connect.tsx
+++ frontend/ecs-react/src/pages/connect.tsx
@@ -40,7 +40,7 @@ function Connect() {
     // base64로 보냄
     const response: AxiosResponse = await axios.post(
       // "https://k8d204.p.ssafy.io/flask/position",
-      "http://ecs_fastapi:5000/flask/position",
+      "https://ecs_fastapi:5000/flask/position",
       // "http://192.168.100.207:5000/flask/position",
       { image: true, buffer: imageSrc, userNo: userNumber }
     )
