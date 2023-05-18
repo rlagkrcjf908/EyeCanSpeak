540d6b4671127a0fc2887a59915a5619f9229a79
dnjs0236 <dnjs0236@gmail.com>
2023-05-18 17:32:42 +0900

    [FE] test

--- frontend/ecs-react/src/pages/setting.tsx
+++ frontend/ecs-react/src/pages/setting.tsx
@@ -46,7 +46,6 @@ export default function Setting() {
     const response: AxiosResponse = await axios.post(
       // api 주소 적기
       "https://localhost:5000/flask/setting",
-
       // `https://k8d204.p.ssafy.io/flask/setting`,
       // `http://192.168.100.207:5000/flask/setting`,
       { userNo: userNumber, imgSrc: imageSrc, index: currentCircle + 1 }
