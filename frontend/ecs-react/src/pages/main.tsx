61c1af87d19486a5e98ebe969b36d97baff0e66b
dnjs0236 <dnjs0236@gmail.com>
2023-05-19 09:21:45 +0900

    [FE] test

--- frontend/ecs-react/src/pages/main.tsx
+++ frontend/ecs-react/src/pages/main.tsx
@@ -3,20 +3,16 @@ import { parseJwt } from "../services/jwtDecode"
 import button from "../assets/image/kakao_button.png"
 import Title from "../components/main/title"
 import style from "../styles/main/main.module.css"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Cookies } from "react-cookie"
-import { isLog, userName, userNo } from "../recoil/atoms/userState"
-import { useRecoilState, useSetRecoilState } from "recoil"
+import { useRecoilState } from "recoil"
 import HelpModal from "../components/modal/helpModal"
 import helpIcon from "../assets/help-bubble.png"
 import { helpModal } from "../recoil/atoms/modalState"
 export default function Main() {
-  const setUserNo = useSetRecoilState(userNo)
-  const setUserName = useSetRecoilState(userName)
-  const [log, setLog] = useRecoilState(isLog)
-
   const navigate = useNavigate()
   const cookies = new Cookies()
+  const userNo = sessionStorage.getItem("userNo")
 
   const [modal, setModal] = useRecoilState(helpModal)
 
@@ -25,20 +21,19 @@ export default function Main() {
   }
 
   useEffect(() => {
+    console.log(userNo)
     const token = cookies.get("accessToken")
     if (token !== undefined) {
       const obj = parseJwt(token)
-      setUserNo(obj.no)
-      setUserName(obj.name)
-      setLog(true)
+      sessionStorage.setItem("userNo", obj.no)
+      sessionStorage.setItem("userName", obj.name)
     }
   }, [])
   return (
     <div className={style.continer}>
       <div className={style.wrapper}>
         <Title></Title>
-        {/* <Content></Content> */}
-        {log ? (
+        {userNo !== null ? (
           <div className={style.box}>
             <div className={style.btnBox}>
               <button
